5014080f52b02ef511cb0d61d98e5fcf8748c05a
Rich-P-Martin <richard.martin@gov.bc.ca>
2023-09-13 13:20:33 -0700

    prior to major changes

--- 01_process.R
+++ 01_process.R
@@ -9,23 +9,13 @@ read_data <- function(file_name){
     clean_names()%>%
     select(o_net_soc_code, element_name, scale_name, data_value)%>%
     pivot_wider(names_from = scale_name, values_from = data_value)%>%
-    mutate(score=sqrt(Importance*Level), #geometric mean of importance and level
+    #mutate(score=sqrt(Importance*Level), #geometric mean of importance and level
+    mutate(score=Level,
            category=(str_split(file_name,"\\.")[[1]][1]))%>%
     unite(element_name, category, element_name, sep=": ")%>%
     select(-Importance, -Level)
 }
-# read_ete <- function(file_name){
-#   read_excel(here("raw_data", "onet", file_name))%>%
-#     clean_names()%>%
-#     select(o_net_soc_code, element_name, category, data_value)%>%
-#     filter(element_name %in% c("Required Level of Education", "Related Work Experience"))%>%
-#     mutate(catval=category*data_value/100)%>% #apply weights to categories
-#     group_by(o_net_soc_code, element_name)%>%
-#     summarize(score=sum(catval, na.rm = TRUE))%>% #weighted mean
-#     mutate(element_name=if_else(element_name == "Required Level of Education",
-#                                 paste("Education", element_name, sep=": "),
-#                                 paste("Experience", element_name, sep=": ")))
-#            }
+
 
 # the program------------------------
 mapping <- read_excel(here("mapping", "onet2019_soc2018_noc2016_noc2021_crosswalk.xlsx"))%>%
@@ -33,11 +23,9 @@ mapping <- read_excel(here("mapping", "onet2019_soc2018_noc2016_noc2021_crosswal
   unite(noc, noc2021, noc2021_title, sep=": ")%>%
   select(noc, o_net_soc_code = onetsoc2019)%>%
   distinct()
+
 tbbl <- tibble(file=c("Skills.xlsx", "Abilities.xlsx", "Knowledge.xlsx", "Work Activities.xlsx"))%>%
   mutate(data=map(file, read_data))%>%
-# tbbl2 <- tibble(file="Education, Training, and Experience.xlsx")%>%
-#   mutate(data=map(file, read_ete))
-#tbbl <- bind_rows(tbbl1, tbbl2)%>%
   select(-file)%>%
   unnest(data)%>%
   pivot_wider(id_cols = o_net_soc_code, names_from = element_name, values_from = score)%>%
@@ -47,14 +35,16 @@ tbbl <- tibble(file=c("Skills.xlsx", "Abilities.xlsx", "Knowledge.xlsx", "Work A
   select(noc, everything())%>%
   group_by(noc)%>%
   summarise(across(where(is.numeric), \(x) mean(x, na.rm = TRUE)))%>% #mapping from SOC to NOC is not one to one: mean give one value per NOC
-#  mutate("Educ. Train. Exper.: TEER"= as.numeric(str_sub(noc, 2, 2)))%>% #NOPE teer is an un-ordered factor, not numeric
-  mutate(across(where(is.numeric), ~ if_else(is.na(.), mean(., na.rm=TRUE), .)))%>% #for 11 occupations and 4 variables replace missing values with the mean
+  mutate(across(where(is.numeric), ~ if_else(is.na(.), mean(., na.rm=TRUE), .))) #for 11 occupations and 4 variables replace missing values with the mean
+
+tbbl%>%
+  write_csv(file=here("processed_data", "unscaled_characteristics_noc.csv"))
+
+tbbl%>%
   column_to_rownames(var="noc")%>%
   scale()%>%
   as.data.frame()%>%
-  rownames_to_column(var="noc")
-
-tbbl%>%
+  rownames_to_column(var="noc")%>%
   write_csv(file=here("processed_data", "scaled_characteristics_noc.csv"))
 
 #make some fake wage data-----------
